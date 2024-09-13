3edac8a73cfe2d3472b49162aab0b4e04349ed54
kachanovski <kachanovski03@gmail.com>
2024-09-13 23:54:01 +0300

    init

---
+++ src/1-app/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css'
+
+export const App  = () => {
+    return (
+      <div className={styles.root}>
+          Click on the Vite and React logos to learn more
+      </div>
+    )
+}
